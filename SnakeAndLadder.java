1f72245eeba4f8e07fcfd2688bc843f490f91771
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 20:07:39 +0530

    Start the Game with Start Position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+/**
+ * SnakeAndLadder
+ */
+public class SnakeAndLadder {
+    // UC1 Initilize Starting and Ending Point
+    static final int START_POS = 0;
+    static final int END_POS = 100;
+
+    public static void main(String[] args) {
+
+    }
+
+}
