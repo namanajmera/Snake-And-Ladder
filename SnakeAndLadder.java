f3c98c649154f614eba12bc73ea37b9b46f7d6b4
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 20:32:09 +0530

    Play with 2 Players.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,58 +7,87 @@ public class SnakeAndLadder {
     // UC1 Initilize Starting and Ending Point
     static final int START_POS = 0;
     static final int END_POS = 100;
-
-    // UC3 User chekcs for plays
+    static Random random = new Random();
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
+    static String task = null;
 
-    public static void GamePlayed() {
-        // UC2 Rolling the dice to get a number between 1 to 6
-        Random random = new Random();
-        int current_position = START_POS;
-        String task = null;
-        int total_dice_count = 0;
-        // UC4 Loop until reaches to Ending Position
-        while (current_position < END_POS) {
+    public static int GamePlayed(int current_position, int diceCount) {
+        if (current_position < END_POS) {
 
             int DICE = random.nextInt(6) + 1;
-            // System.out.println("Rolling dice: " + DICE);
 
             int action = random.nextInt(3);
-            // System.out.println("Action dice: " + action);
-            total_dice_count++;
-            // UC3 Check the play.
 
-            if (action == LADDER && (current_position + DICE <= END_POS)) {
+            switch (action) {
+            case LADDER:
+                if (action == LADDER && (current_position + DICE <= END_POS)) {
+                    current_position = current_position + DICE;
+                    task = "Ladder";
+                }
+                break;
+            case SNAKE:
+                if (action == SNAKE && (current_position - DICE >= START_POS)) {
+                    current_position = current_position - DICE;
+                    task = "Snake";
+                }
 
-                current_position = current_position + DICE;
-                task = "Ladder";
-                // System.out.println("Current position is: " + current_position);
-
-            } else if (action == SNAKE && (current_position - DICE >= START_POS)) {
-
-                current_position = current_position - DICE;
-                task = "Snake";
-                // System.out.println("Current position is: " + current_position);
-
-            } else if (action == NO_PLAY) {
-                task = "No Play";
-                // System.out.println("No Play");
+            default:
+                if (action == NO_PLAY) {
+                    task = "No Play";
+                }
+                break;
             }
             System.out.println("| Dice:" + DICE + " " + task + " and Current position:" + current_position + " |");
+
+            if (task == "Ladder" && current_position != END_POS) {
+                diceCount = DiceCount(diceCount);
+                GamePlayed(current_position, diceCount);
+            }
+
         }
-        // UC5 The player gets excat wining
-        System.out.println();
-        System.out.println(
-                "----------------------------------------------------------------------------------------------------");
-        System.out.println("Congratulation");
-        System.out.println("You win the game. ");
-        System.out.println("Your Position is: " + current_position);
-        System.out.println("Total Count of Dice: " + total_dice_count);
+        return current_position;
+    }
+
+    public static int DiceCount(int diceCount) {
+        diceCount++;
+        return diceCount;
+    }
+
+    public static void twoPlayers() {
+        int Player1Position = START_POS;
+        int Player2Position = START_POS;
+        int diceCount = START_POS;
+        System.out.println("Game started by two Players.");
+        while (Player1Position < END_POS && Player2Position < END_POS) {
+            System.out.println("Player 1:- ");
+            Player1Position = GamePlayed(Player1Position, diceCount);
+            diceCount = DiceCount(diceCount);
+            System.out.println(
+                    "----------------------------------------------------------------------------------------------");
+            if (Player1Position == END_POS) {
+                break;
+            }
+
+            System.out.println("Player 2:- ");
+            Player2Position = GamePlayed(Player2Position, diceCount);
+            diceCount = DiceCount(diceCount);
+            System.out.println(
+                    "----------------------------------------------------------------------------------------------");
+        }
+        if (Player1Position == END_POS) {
+            System.out.println();
+            System.out.println("Player 1 Win.");
+        } else if (Player2Position == END_POS) {
+            System.out.println();
+            System.out.println("Player 2 Win.");
+        }
+        System.out.println("Total dice played by both player is:- " + diceCount);
     }
 
     public static void main(String[] args) {
-        GamePlayed();
+        twoPlayers();
     }
+
 }
